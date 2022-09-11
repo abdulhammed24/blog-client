f59fe35585239429803f74e84dc2f7a872efee82
abdulhammed24 <abdlhamd3@gmail.com>
2022-09-11 14:34:38 -0700

    frontend for blog

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,4 +1,4 @@
-// const baseUrlLocal = "http://localhost:5000";
+const baseUrl = "https://blog-server-v1.herokuapp.com/";
 
-const baseUrl = process.env.REACT_APP_API_URL;
+// const baseUrl = process.env.REACT_APP_API_URL;
 export default baseUrl;
