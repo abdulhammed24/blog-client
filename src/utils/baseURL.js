fdd70fe15b58671a2792f3c35381b7f3d64470ab
abdulhammed24 <abdlhamd3@gmail.com>
2022-12-11 13:05:20 +0100

    corrected some code

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,4 +1,4 @@
-const baseUrl = "https://blog-server-v1.herokuapp.com";
+const baseUrl = "https://blog-server-v1.herokuapp.com/";
 
 // const baseUrl = "http://localhost:8000";
 export default baseUrl;
